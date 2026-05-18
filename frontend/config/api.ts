7d57a0dc956f7db8bf777c8bb3e897e29700bbf5
Nagaraj <nagaraj@Nagarajs-MacBook-Air.local>
2026-05-19 04:40:02 +0530

    Bug fixing

--- frontend/config/api.ts
+++ frontend/config/api.ts
@@ -1,7 +1,7 @@
 const rawApiHost =
   process.env.NEXT_PUBLIC_API_HOST
     ?.trim()
-    .replace(/\/+$/g, "") || "stock.eatoo.in";
+    .replace(/\/+$/g, "") || "stock.eatoo.in/api";
  
 const apiHost = rawApiHost;
 
