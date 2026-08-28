3a0af49f1cb740c6e091bc926964164713309816
Nagaraj <nagaraj@Nagarajs-MacBook-Air.local>
2026-08-28 13:34:44 +0530

    b

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     NAG: "https://stock.eatoo.in/api",
-    CUTIE: "https://stock2.eatoo.in/api",
+    CUTIE: "https://stock1.eatoo.in/api",
     STOCK: "https://stock.eatoo.in/api",
 };
 
